5ee6174d8a83c48deb1e8401fe0e5a843d020138
Nada Haimn <nadahaimn2170@gmail.com>
2025-08-09 11:10:14 +0300

    New

---
+++ src/models/enums/ResponseEnums.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class ResponseSignal(Enum):
+
+    FILE_VALIDATION_SUCCESS = "File validation successful"
+    FILE_TYPE_NOT_SUPPORTED = "File type is not supported"
+    FILE_SIZE_EXCEEDED = "File size exceeded the limit"
+    FILE_UPLOAD_SUCCESS = "File uploaded successfully"
+    FILE_UPLOAD_FAILED = "File upload failed"
