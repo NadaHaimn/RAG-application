ac36487353c9ba4d011c9247ba54f94d97921286
Nada Haimn <nadahaimn2170@gmail.com>
2025-08-10 05:52:05 +0300

    updating

--- src/routes/data.py
+++ src/routes/data.py
@@ -2,7 +2,13 @@ from fastapi import FastAPI, APIRouter,Depends, UploadFile , status
 from fastapi.responses import JSONResponse
 import os 
 from helpers.config import get_settings , Settings 
-from controllers import DataController
+from controllers import DataController , ProjectController
+import aiofiles
+from models import ResponseSignal
+import logging
+
+
+logger = logging.getLogger('uvicorn.error')
 
 data_router = APIRouter(
     prefix="/api/v1/data",
@@ -12,12 +18,40 @@ data_router = APIRouter(
 @data_router.post("/upload/{project_id}")
 async def upload_data(project_id: str , file:UploadFile,
                       app_settings:Settings = Depends(get_settings)):
-
     # Validate the file properties
-    is_valid , result_signal = DataController().validate_file_properties(file=file)
+    data_controller = DataController()
+    is_valid , result_signal = data_controller.validate_file_properties(file=file)
 
     if not is_valid:
         return JSONResponse(
             status_code=status.HTTP_400_BAD_REQUEST,
             content={"message": result_signal}
+        )
+    
+
+    # Get the project path
+    project_dir_path = ProjectController().get_project_path(project_id=project_id)
+    file_path = data_controller.generate_unique_filename(
+        orig_file_name=file.filename,
+        project_id=project_id
+    )
+
+    try:
+        async with aiofiles.open(file_path, 'wb') as f:
+            while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
+                await f.write(chunk)
+
+    except Exception as e:
+
+        logger.error(f"Error while uploading file : {e}")
+
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={"message": ResponseSignal.FILE_UPLOAD_FAILED.value }
+        )
+
+    return JSONResponse(
+            content={
+                "message": ResponseSignal.FILE_UPLOAD_SUCCESS.value
+            }
         )
