649528a657f9b7f3124bf6bb58e7131f92c1ea73
Nada Haimn <nadahaimn2170@gmail.com>
2025-08-10 05:51:08 +0300

    updating

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -1,6 +1,9 @@
+from .ProjectController import ProjectController
 from .BaseController import BaseController
 from fastapi import FastAPI, APIRouter,Depends, UploadFile
 from models import ResponseSignal
+import os
+import re
 
 class DataController(BaseController):
     
@@ -17,3 +20,37 @@ class DataController(BaseController):
             return False , ResponseSignal.FILE_SIZE_EXCEEDED.value
 
         return True , ResponseSignal.FILE_VALIDATION_SUCCESS.value
+
+    
+    def generate_unique_filename(self , orig_file_name: str , project_id: str):
+
+        random_key = self.generate_random_string()
+        project_path = ProjectController().get_project_path(project_id=project_id)
+        cleaned_file_name = self.get_clean_file_name(
+            orig_file_name=orig_file_name
+        )
+
+        # Create a unique file name by combining the cleaned file name with a random string
+        new_file_name = os.path.join(
+            project_path, 
+            f"{random_key}_{cleaned_file_name}"
+        )
+
+        while os.path.exists(new_file_name):
+            random_key = self.generate_random_string()
+            new_file_name = os.path.join(
+                project_path, 
+                f"{random_key}_{cleaned_file_name}"
+            )
+
+        return new_file_name
+
+
+    def get_clean_file_name(self, orig_file_name: str):
+        # Remove any special characters except for dot and underscore
+        cleaned_file_name = re.sub(r'[^\w.]','', orig_file_name.strip())
+
+        # replace spaces with underscores
+        cleaned_file_name = cleaned_file_name.replace(" ", "_")
+
+        return cleaned_file_name
