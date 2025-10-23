5d57a4aabf6053d72dd415d8755b275cf6d1b51b
Nada Haimn <nadahaimn2170@gmail.com>
2025-10-23 05:36:06 +0300

    fix: update ProjectModel initialization and improve project ID assignment

--- src/models/ProjectModel.py
+++ src/models/ProjectModel.py
@@ -6,9 +6,9 @@ from .enums.DataBaseEnum import DataBaseEnum
 class ProjectModel(BaseDataModel):
 
     def __init__(self, db_client: object):
-        super().__init__(db_client)
+        super().__init__(db_client=db_client)
         self.collection = self.db_client[DataBaseEnum.COLLECTION_PROJECT_NAME.value]   
-        self.app_settings = Project
+        #self.app_settings = Project
         
     @classmethod
     async def create_instance(cls,db_client:object):
@@ -30,8 +30,8 @@ class ProjectModel(BaseDataModel):
 
     async def create_project(self, project: Project):
 
-        result = await self.collection.insert_one(project.dict())
-        project._id = result.inserted_id
+        result = await self.collection.insert_one(project.dict(by_alias=True, exclude_unset=True))
+        project.id = result.inserted_id
         return project
 
     async def get_project_or_create(self, project_id: str):
