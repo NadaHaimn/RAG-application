874f88f1a1ae43a8dbc2f6fc86c6cbbaf5389f81
Nada Haimn <nadahaimn2170@gmail.com>
2025-10-23 05:35:44 +0300

    fix: correct typo in validate_file_properties method and improve unique file path generation

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -10,7 +10,7 @@ class DataController(BaseController):
     def __init__(self):
         super().__init__()
     
-    # validate the file properities
+    # validate the file properties
     def validate_file_properties(self, file: UploadFile):
         
         if file.content_type not in self.app_settings.FILE_ALLOWED_TYPES:
@@ -22,28 +22,29 @@ class DataController(BaseController):
         return True , ResponseSignal.FILE_VALIDATION_SUCCESS.value
     
     
-    def generate_unique_filepath(self , orig_file_name: str , project_id: str):
+    def generate_unique_filepath(self, orig_file_name: str, project_id: str):
 
         random_key = self.generate_random_string()
         project_path = ProjectController().get_project_path(project_id=project_id)
+
         cleaned_file_name = self.get_clean_file_name(
             orig_file_name=orig_file_name
         )
 
-        # Create a unique file name by combining the cleaned file name with a random string
-        new_file_name = os.path.join(
-            project_path, 
-            f"{random_key}_{cleaned_file_name}"
+        new_file_path = os.path.join(
+            project_path,
+            random_key + "_" + cleaned_file_name
         )
 
-        while os.path.exists(new_file_name):
+        while os.path.exists(new_file_path):
             random_key = self.generate_random_string()
-            new_file_name = os.path.join(
-                project_path, 
-                f"{random_key}_{cleaned_file_name}"
+            new_file_path = os.path.join(
+                project_path,
+                random_key + "_" + cleaned_file_name
             )
 
-        return new_file_name, f"{random_key}_{cleaned_file_name}"
+        return new_file_path, random_key + "_" + cleaned_file_name
+    
 
 
     def get_clean_file_name(self, orig_file_name: str):
